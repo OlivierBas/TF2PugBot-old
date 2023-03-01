0886d2ac6852fe8fa7b475f211ff6c961582c2bf
Olivier <92113985+OlivierDotNet@users.noreply.github.com>
2023-03-01 15:46:51 +0100

    Split all DataManagers

---
+++ Data/BaseDataManager.cs
@@ -0,0 +1,229 @@
+﻿using System.Text;
+using System.Text.Json;
+using Discord.WebSocket;
+using TF2PugBot.Extensions;
+using TF2PugBot.Types;
+
+namespace TF2PugBot.Data;
+
+public static partial class DataManager
+{
+    private static string _token = string.Empty;
+    private static bool _instantSpin;
+    private static ulong _devId;
+
+    public static string Token
+    {
+        get => _token;
+        set
+        {
+            if (string.IsNullOrEmpty(_token))
+            {
+                _token = value;
+            }
+        }
+    }
+
+    public static bool InstantSpin
+    {
+        get => _instantSpin;
+        set => _instantSpin = value;
+    } 
+    
+    public static ulong DevId
+    {
+        get => _devId;
+        set => _devId = value;
+    }
+
+    private static string MedImmunityDb
+    {
+        get => EasySetup.MedDbFileName + ".json";
+    }
+    
+    private static string GuildDataDb
+    {
+        get => EasySetup.GuildDbFileName + ".json";
+    }
+    
+    private static string PlayerStatsDb
+    {
+        get => EasySetup.StatsDbFileName + ".json";
+    }
+
+    static DataManager ()
+    {
+        RetrieveDb();
+    }
+
+    public static bool HasAccessToCommand (ulong? guildId, SocketGuildUser caller)
+    {
+        var guildData = _guildSettingsData.FirstOrDefault(g => g.GuildId == guildId);
+        if (guildData is not null)
+        {
+            if (caller.Roles.Any(r => r.Id == guildData.AdminRoleId))
+            {
+                return true;
+            }
+
+            if (caller.Roles.Any(r => r.Permissions.Administrator))
+            {
+                return true;
+            }
+
+            if (caller.Guild.OwnerId == caller.Id)
+            {
+                return true;
+            }
+
+            if (caller.Id == _devId)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static async Task RetrieveDbAsync ()
+    {
+
+        await using (FileStream fs = new FileStream(MedImmunityDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = await sr.ReadToEndAsync();
+                if (data.Length > 0)
+                {
+                    _medImmunities = JsonSerializer.Deserialize<List<MedicImmunePlayer>>(data)!;
+                }
+            }
+        }
+        
+        await using (FileStream fs = new FileStream(GuildDataDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = await sr.ReadToEndAsync();
+                if (data.Length > 0)
+                {
+                    _guildSettingsData = JsonSerializer.Deserialize<List<GuildSettingsData>>(data)!;
+                }
+            }
+        }
+        
+        await using (FileStream fs = new FileStream(PlayerStatsDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = await sr.ReadToEndAsync();
+                if (data.Length > 0)
+                {
+                    _playerStats = JsonSerializer.Deserialize<List<PlayerStats>>(data)!;
+                }
+            }
+        }
+    }
+
+    private static void RetrieveDb ()
+    {
+
+        
+        using (FileStream fs = new FileStream(MedImmunityDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = sr.ReadToEnd();
+                if (data.Length > 0)
+                {
+                    _medImmunities = JsonSerializer.Deserialize<List<MedicImmunePlayer>>(data)!;
+                }
+            }
+        }
+        
+        using (FileStream fs = new FileStream(GuildDataDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = sr.ReadToEnd();
+                if (data.Length > 0)
+                {
+                    _guildSettingsData = JsonSerializer.Deserialize<List<GuildSettingsData>>(data)!;
+                }
+            }
+        }
+        
+        using (FileStream fs = new FileStream(PlayerStatsDb, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                string data = sr.ReadToEnd();
+                if (data.Length > 0)
+                {
+                    _playerStats = JsonSerializer.Deserialize<List<PlayerStats>>(data)!;
+                }
+            }
+        }
+    }
+
+    private static async Task SaveDbAsync ()
+    {
+
+        
+        await using (FileStream fs = new FileStream(MedImmunityDb, FileMode.Truncate, FileAccess.Write))
+        {
+            await using (StreamWriter sw = new StreamWriter(fs))
+            {
+                var storedImmunities =_medImmunities.DistinctBy(mi => new { mi.Id, mi.GuildId }).ToList();
+                await sw.WriteAsync(JsonSerializer.Serialize(storedImmunities));
+            }
+        }
+        
+        await using (FileStream fs = new FileStream(GuildDataDb, FileMode.Truncate, FileAccess.Write))
+        {
+            await using (StreamWriter sw = new StreamWriter(fs))
+            {
+                await sw.WriteAsync(JsonSerializer.Serialize(_guildSettingsData));
+            }
+        }
+        
+        await using (FileStream fs = new FileStream(PlayerStatsDb, FileMode.Truncate, FileAccess.Write))
+        {
+            await using (StreamWriter sw = new StreamWriter(fs))
+            {
+                await sw.WriteAsync(JsonSerializer.Serialize(_playerStats));
+            }
+        }
+    }
+
+    private static void SaveDb ()
+    {
+
+        using (FileStream fs = new FileStream(MedImmunityDb, FileMode.Truncate, FileAccess.Write))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                var storedImmunities =_medImmunities.DistinctBy(mi => new { mi.Id, mi.GuildId }).ToList();
+                sw.Write(JsonSerializer.Serialize(storedImmunities));
+            }
+        }
+        
+        using (FileStream fs = new FileStream(GuildDataDb, FileMode.Truncate, FileAccess.Write))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.Write(JsonSerializer.Serialize(_guildSettingsData));
+            }
+        }
+        
+        using (FileStream fs = new FileStream(PlayerStatsDb, FileMode.Truncate, FileAccess.Write))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.Write(JsonSerializer.Serialize(_playerStats));
+            }
+        }
+    }
+
+
+}
---
+++ Data/MedTrackerDataManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using System.Text.Json;
+using Discord.WebSocket;
+using TF2PugBot.Extensions;
+using TF2PugBot.Types;
+
+namespace TF2PugBot.Data;
+
+public static partial class DataManager
+{
+    private static List<MedicImmunePlayer> _medImmunities = new List<MedicImmunePlayer> ();
+    private static Dictionary<ulong, MedicImmunePlayer[]> _temporaryMedicImmunities = new Dictionary<ulong, MedicImmunePlayer[]>();
+    
+    public static IReadOnlyCollection<MedicImmunePlayer> TrackedMedImmunities => _medImmunities.AsReadOnly();
+
+    public static IReadOnlyCollection<MedicImmunePlayer> GetMedImmunePlayers (ulong? guildId)
+    {
+        List<MedicImmunePlayer> toBeRemoved =_medImmunities.Where(p => p.Added.HoursFromNow() < 0 && p.GuildId == guildId).ToList();
+        if (toBeRemoved.Count > 0)
+        {
+            _medImmunities.RemoveAll(p => toBeRemoved.Contains(p));
+
+        }
+        SaveDb();
+        return _medImmunities;
+    }
+
+    public static IReadOnlyCollection<MedicImmunePlayer> GetTemporaryMedImmunePlayers (ulong? guildId)
+    {
+        return _temporaryMedicImmunities.FirstOrDefault(m => m.Key == guildId).Value.ToList();
+    }
+
+    public static void MakePlayerMedImmune (SocketGuildUser player, Team team)
+    {
+        var medicImmunePlayer = new MedicImmunePlayer()
+        {
+            DisplayName = player.DisplayName,
+            Id          = player.Id,
+            GuildId     = player.Guild.Id,
+            Added       = DateTime.Now
+        };
+        
+        switch (team)
+        {
+            case Team.RED:
+                _temporaryMedicImmunities[player.Guild.Id][(int)team] = medicImmunePlayer;
+                break;
+            case Team.BLU:
+                _temporaryMedicImmunities[player.Guild.Id][(int)team] = medicImmunePlayer;
+                break;
+        }
+        
+    }
+
+    public static async Task MakePermanentImmunitiesAsync (ulong guildId)
+    {
+        foreach (var medicImmunePlayer in _temporaryMedicImmunities[guildId])
+        {
+            if (medicImmunePlayer.Added.MinutesFromNow() > 4)
+            {
+                if (_medImmunities.Contains(medicImmunePlayer))
+                {
+                    _medImmunities.FirstOrDefault(p => p.Id == medicImmunePlayer.Id && p.GuildId == medicImmunePlayer.GuildId)!.Added = DateTime.Now;
+                    continue;
+                }
+                _medImmunities.Add(medicImmunePlayer);
+            }
+        }
+        _temporaryMedicImmunities[guildId] = new MedicImmunePlayer[2];
+        await SaveDbAsync();
+    }
+
+    public static string GetMedImmunePlayerString (ulong? guildId)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var player in _medImmunities.Where(p => p.GuildId == guildId))
+        {
+            sb.Append($"{player.DisplayName} has med immunity for {12 - player.Added.HoursFromNow()} hours\n");
+        }
+        return sb.ToString();
+    }
+
+    public static async Task ClearListOfImmunePlayersAsync (List<SocketGuildUser> playersToBeRemoved)
+    {
+        _medImmunities.RemoveAll(m => playersToBeRemoved.Select(p => (MedicImmunePlayer)p).Contains(m));
+        await SaveDbAsync();
+    }
+
+    public static async Task ForceAddMedImmunePlayerAsync (SocketGuildUser player)
+    {
+        _medImmunities.Add(player);
+        await SaveDbAsync();
+    }
+
+    public static async Task ForceRemoveMedImmunePlayerAsync (SocketGuildUser player)
+    {
+        _medImmunities.RemoveAll(m => m.GuildId == player.Guild.Id && m.Id == player.Id);
+        await SaveDbAsync();
+    }
+    
+    
+
+}
