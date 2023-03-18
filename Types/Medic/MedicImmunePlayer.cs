f80be8bc6e26723a32da63b5e28ba49b3a6be2d0
Olivier <92113985+OlivierBas@users.noreply.github.com>
2023-03-18 19:38:33 +0100

    Actually fix duplicate medic immunities

--- Types/Medic/MedicImmunePlayer.cs
+++ Types/Medic/MedicImmunePlayer.cs
@@ -11,4 +11,15 @@ public class MedicImmunePlayer
 
     public static implicit operator MedicImmunePlayer (SocketGuildUser user) => new MedicImmunePlayer()
         { Id = user.Id, DisplayName = user.DisplayName, GuildId = user.Guild.Id };
+
+    /// <inheritdoc />
+    public override bool Equals (object? obj)
+    {
+        var mip = obj as MedicImmunePlayer;
+        if (mip != null)
+        {
+            return mip.Id == Id && mip.GuildId == GuildId;
+        }
+        return base.Equals(obj);
+    }
 }
