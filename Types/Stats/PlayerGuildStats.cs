d7ace5eb7bb883e16073699dd70a96791a290f1b
Olivier <92113985+OlivierBas@users.noreply.github.com>
2023-04-13 09:32:44 +0200

    Just return 0% if there are no stats

--- Types/Stats/PlayerGuildStats.cs
+++ Types/Stats/PlayerGuildStats.cs
@@ -8,6 +8,10 @@ public class PlayerGuildStats
     public int WonCaptainSpins { get; set; }
     public DateTime LastPlayed { get; set; } = DateTime.Now;
 
-    public float MedSpinsWonPercentage => (float)WonMedicSpins / GamesPlayed;
-    public float CaptainSpinsWonPercentage => (float)WonCaptainSpins / GamesPlayed;
+        public float MedSpinsWonPercentage => (GamesPlayed > 0 && WonMedicSpins > 0) ?
+                                                (float)WonMedicSpins / GamesPlayed :
+                                                0.0f; 
+        public float CaptainSpinsWonPercentage => (GamesPlayed > 0 && WonCaptainSpins > 0) ?
+                                                    (float)WonCaptainSpins / GamesPlayed :
+                                                    0.0f;
 }
